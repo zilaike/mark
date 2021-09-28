8d05f9273596f5b8ce10cf4ff36440da0b5610e0
朱鹏举 <zhupj@missfresh.cn>
2021-09-28 17:45:07 +0800

    替换

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,7 +14,9 @@ iptables -A INPUT -i ${VPN_DEVICE} -p esp -j ACCEPT
 iptables -A INPUT -i ${VPN_DEVICE} -p udp --dport 500 -j ACCEPT
 iptables -A INPUT -i ${VPN_DEVICE} -p tcp --dport 500 -j ACCEPT
 iptables -A INPUT -i ${VPN_DEVICE} -p udp --dport 4500 -j ACCEPT
-iptables -A INPUT -i ${VPN_DEVICE} -p udp --dport 1701 -j ACCEPT
-iptables -A INPUT -i ${VPN_DEVICE} -p tcp --dport 1723 -j ACCEPT
+#L2TP
+#iptables -A INPUT -i ${VPN_DEVICE} -p udp --dport 1701 -j ACCEPT
+#PPTP
+#iptables -A INPUT -i ${VPN_DEVICE} -p tcp --dport 1723 -j ACCEPT
 iptables -t nat -A POSTROUTING -s ${VPN_NETWORK} -o ${VPN_DEVICE} -j MASQUERADE
 exec ipsec start --nofork "$@"
