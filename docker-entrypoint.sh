07b2ec26c4fc50af76ff42c64fe340b69fd8572a
朱鹏举 <zhupj@missfresh.cn>
2021-09-28 20:44:23 +0800

    原始

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,23 +6,6 @@
 # - VPN_NETWORK
 #
 
-/init.sh
 
-exec  cat > /etc/sysctl.d/10-ipsec.conf<<-EOF
-net.ipv4.ip_forward=1
-EOF
-exec  sysctl --system
-
-
-iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -s ${VPN_NETWORK}  -j ACCEPT
-iptables -A INPUT -i ${VPN_DEVICE} -p esp -j ACCEPT
-iptables -A INPUT -i ${VPN_DEVICE} -p udp --dport 500 -j ACCEPT
-iptables -A INPUT -i ${VPN_DEVICE} -p tcp --dport 500 -j ACCEPT
-iptables -A INPUT -i ${VPN_DEVICE} -p udp --dport 4500 -j ACCEPT
-#L2TP
-#iptables -A INPUT -i ${VPN_DEVICE} -p udp --dport 1701 -j ACCEPT
-#PPTP
-#iptables -A INPUT -i ${VPN_DEVICE} -p tcp --dport 1723 -j ACCEPT
 iptables -t nat -A POSTROUTING -s ${VPN_NETWORK} -o ${VPN_DEVICE} -j MASQUERADE
 exec ipsec start --nofork "$@"
